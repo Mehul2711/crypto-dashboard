78cf012ffa92212d960a567630445caca576da81
Mehul Kumar <mehul271102@gmail.com>
2025-06-13 00:50:40 +0530

    🚑️ Fix cors

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
     try {
       setLoading(true);
       const res = await axios.get(
-        "https://thingproxy.freeboard.io/fetch/https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true"
       );
       console.log("Coins:", res.data);
       setCoins(res.data);
